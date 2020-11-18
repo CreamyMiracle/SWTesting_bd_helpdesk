b281a3422d0a724ae79553361d5982bc9c5a6f09
Joonas Hyvärinen <joonas.hyvarinen@tuni.fi>
2020-11-18 17:01:36 +0200

    Fix bug

--- src/add.js
+++ src/add.js
@@ -15,5 +15,4 @@ import createMathOperation from './.internal/createMathOperation.js'
  */
 const add = createMathOperation((augend, addend) => augend + addend, 0)
 
-//export default add
-module.exports = add
+export default add
